32242e5f1c65665f0beab0c0b8a611b8789e5372
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 01:58:13 +0200

    Update 5-size.js

--- Tasks/size.js
+++ Tasks/size.js
@@ -1,9 +1,10 @@
+const sizeToString = (value, unit) =>`${value} ${SIZE_UNITS[unit]}`;
 const size = (bytes)=> {
-  if (bytes === 0) return '0' + SIZE_UNITS[0];
+  if (bytes === 0) return sizeToString(bytes,0);
     const exp = Math.floor(Math.log(bytes) / Math.log(1000));
 const value = Math.round(bytes / 1000**exp);
-    const unit =SIZE_UNITS[exp];
-   return value.toString()+ ' ' +unit;
+   
+   return sizeToString(value,exp);
 };
 
 module.exports = size;
