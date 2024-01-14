94415a55f9eb1c06f9e58700719c6c5dc1a41665
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 01:57:55 +0200

    Update 4-size.js

--- Tasks/size.js
+++ Tasks/size.js
@@ -1,8 +1,8 @@
 const size = (bytes)=> {
-  if (bytes === 0) return '0 byte';
+  if (bytes === 0) return '0' + SIZE_UNITS[0];
     const exp = Math.floor(Math.log(bytes) / Math.log(1000));
 const value = Math.round(bytes / 1000**exp);
-    const unit = exp ===0? 'byte' : SIZE_UNITS[exp-1];
+    const unit =SIZE_UNITS[exp];
    return value.toString()+ ' ' +unit;
 };
 
