51372ae1dd3afe0f9bf93b49e49c6798ff852252
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 01:57:23 +0200

    Update 2-size.js

--- Tasks/size.js
+++ Tasks/size.js
@@ -1,12 +1,13 @@
-const size = (size)=> {
-  if (size === 0) return '0 byte';
+const size = (bytes)=> {
+  if (bytes === 0) return '0 byte';
   else {
-    const exp = Math.floor(Math.log(size) / Math.log(1000));
-    if (exp == 0) return size + ' byte';
-    else if (exp == 1) return Math.round(size / 1000) + ' kb';
-    else if (exp == 2) return Math.round(size / 1000000) + ' mb';
-    else if (exp == 3) return Math.round(size / 1000000000) + ' gb';
-  }
+    const exp = Math.floor(Math.log(bytes) / Math.log(1000));
+const value = Math.round(bytes / 1000**exp);
+    let unit =' byte';
+    if (exp == 1) unit = ' kb';
+    if (exp == 2) unit = ' mb';
+    if (exp == 3) unit = ' gb';
+   return value.toString()+unit;
 };
 
 module.exports = size;
