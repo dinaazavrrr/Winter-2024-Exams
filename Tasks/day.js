6974d302a6a5791a7bd80ea2245576292fd5335c
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 01:51:19 +0200

    Update 3-day.js

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,8 +1,8 @@
 const day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 _parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < day.length; i++) {
+  
+  for (let i = 0; i < day.length; i++) {
     if (s.startsWith(day[i].toLowerCase())) {
       return i + 1;
     }
