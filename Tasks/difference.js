d4b395c0613c0bf1c0eba279afc4179fdea9a6eb
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 01:55:50 +0200

    Update 4-difference.js

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,10 +1,5 @@
 const diff = (object1, object2) => {
-  for (const attributeName in object1) {
-    object1[attributeName] = object1[attributeName];
-  }
-  if (object1 in object2) return false;
-  for (attributeName in object2) {
-    object1[attributeName] = object2[attributeName];
+  for (const attributeName in object2) {
     delete object1[attributeName];
   }
   return object1;
