936edffcd3d5cb4c33f9b1db4516c87e9e3a199b
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 02:07:10 +0200

    Update 3-skip.js

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -2,15 +2,7 @@
 
 const removeValues = (arr, ...toRemove)=> {
 const filteredArray = arr.filter(item => !toRemove.includes(item));
-  for (C of T) {
-    for (X of Remove) {
-      if (C === X) {
-        T.splice(x, 1);
-      }
-    }
-    x++;
-  }
-   return filteredArray;
+  return arr.filter(item => !toRemove.includes(item));
 };
 
 module.exports = removeValues;
