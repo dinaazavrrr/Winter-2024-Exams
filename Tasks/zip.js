9dd6988f953cc7061b698e2fe8d94ae549481d7c
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 02:07:57 +0200

    Update 2-zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -3,8 +3,8 @@
 const zipArrays = function (arr1 = [], arr2 = []) {
   const result = [];
   let iindex = 0;
-  for (x of b) {
-    CELL = [a[i++], x];
+for (const val2 of arr2) {
+    const zipped = [arr1[index++], val2];
     if (i < j) {
       delete a[i++];
     } else {
