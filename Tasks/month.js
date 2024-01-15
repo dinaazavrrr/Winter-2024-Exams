7c42fa11ff06eb154163caa63666d974436d7abc
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 02:05:52 +0200

    Update 2-month.js

--- Tasks/month.js
+++ Tasks/month.js
@@ -4,13 +4,11 @@ const getMonthNumber = (input) => {
   const months = [
     'jan', 'feb', 'mar', 'apr', 'may', 'jun',
     'jul', 'aug', 'sep', 'oct', 'nov', 'dec'
-  ];
-Month = (s) => {
-  l = Months.length;
+];
   for (let i = 0; i < l; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
   }
-  return -1;
+ return index !== -1 ? index + 1 : -1;
 };
 
 module.exports = Month;
