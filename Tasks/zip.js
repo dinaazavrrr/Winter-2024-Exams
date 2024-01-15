5e19fd9befd7b7fe02c96bc5311527809960e70e
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 02:08:47 +0200

    Update 5-zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,14 +1,16 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
-
-const zipArrays = function (arr1 = [], arr2 = []) {
-  const result = [];
+const zipArrays = (arr1 = [], arr2 = []) => {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    console.error('Both parameters should be arrays.');
+    return [];
+  }
   const minLength = Math.min(arr1.length, arr2.length);
   for (let i = 0; i < minLength; i++) {
     const zipped = [arr1[i], arr2[i]];
     result.push(zipped);
    
   }
-  return result ;
+ return Array.from({ length: minLength }, (_, i) => [arr1[i], arr2[i]]);
 };
 
 module.exports = zipArrays;
