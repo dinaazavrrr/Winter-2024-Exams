c9bcf98230926bc9cc331b0da7eaad00fffa60af
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 02:08:31 +0200

    Update 4-zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -2,13 +2,13 @@
 
 const zipArrays = function (arr1 = [], arr2 = []) {
   const result = [];
-  let iindex = 0;
-for (const val2 of arr2) {
-    const zipped = [arr1[index++], val2];
-   result.push(zipped);
+  const minLength = Math.min(arr1.length, arr2.length);
+  for (let i = 0; i < minLength; i++) {
+    const zipped = [arr1[i], arr2[i]];
+    result.push(zipped);
    
   }
-  return result.filter(item => item[0] !== undefined);
+  return result ;
 };
 
 module.exports = zipArrays;
