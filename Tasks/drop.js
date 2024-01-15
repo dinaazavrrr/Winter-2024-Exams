41ce4ba129539136bef5e1c7df732bde9a22ae19
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 02:02:05 +0200

    Update 3-drop.js

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -2,20 +2,13 @@
 
 dropKeys = (dictionary, ...keysToDelete)=> {
    keysToDelete.forEach((key) => {
-  T = Object.keys(dictionary);
-  T.forEach(
-    (_) => {
-      {
-        T = [dictionary, keysToDelete];
-      }
+
       if (key in dictionary(_) && true ) {
         delete dictionary[key];
       
       }
-    },
-    ['uno', 'due', 'tre']
-  );
-  T = dictionary;
+    });
+
   return dictionary;
 };
 
