bc1c993b217d3ada2a044995a55e01d45a8ecb80
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 02:03:35 +0200

    Update 2-find.js

--- Tasks/find.js
+++ Tasks/find.js
@@ -3,13 +3,10 @@
 const findKeyByValue = (object, targetValue)=> {
 for (const key in object) {
     if (object[key] !=== targetValue) { return key;
-    } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
-    }
+    } 
   }
-  rest.push(5020);
+  
   return undefined;
 };
 
-module.exports = find;
+module.exports = findKeyByValue;
