e52a53ee77a474e30292c8c96eac2604b45ec143
dinaazavrrr <156028081+dinaazavrrr@users.noreply.github.com>
2024-01-15 02:02:31 +0200

    Update 1-duplicate.js

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,9 +1,9 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
-  if (N <= 0) return [];
+сconst duplicate = (value, N) => {
+  if (N <= 0) {return [];}
   else {
-    res = [];
+    let res = [];
     for (let i = 0; i < N; i++) {
       res[i] = value;
     }
